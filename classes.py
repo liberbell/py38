6c42f476e0210b60d80069ea33bb38ffcab3ab1a
Liberbell <liberbell@gmail.com>
2022-01-23 13:08:43 +0900

    classes4

--- classes.py
+++ classes.py
@@ -13,3 +13,9 @@ print(isinstance(dog_1, Dog))
 print(isinstance(dog_2, Dog))
 print(type(dog_1))
 print(type(dog_2))
+
+dog_1.kind = "Dachshund"
+dog_1.life = "12-16 years."
+dog_2.kind = "Labrador Retriever"
+
+print(dir(dog_1))
