7422d29737ec372282d164f990610745ee573c1c
Liberbell <liberbell@gmail.com>
2022-01-24 09:26:37 +0900

    errors9

--- errors.py
+++ errors.py
@@ -35,10 +35,24 @@ from logging import exception
 
 # print(issubclass(ValueError, Exception))
 
+# try:
+#     user_input = input("Please input an integer: ")
+#     result = 100 / int(user_input)
+#     print("100 divided by the integer you input: ", result)
+# except Exception:
+#     print("Oops an exception was thrown.")
+# print("outside the try/except block")
+
 try:
     user_input = input("Please input an integer: ")
-    result = 100 / int(user_input)
+    result = 100 / user_input
     print("100 divided by the integer you input: ", result)
+except ValueError as ve:
+    print("Oops a Value error was thrown.")
+    print("Error type: ", type(ve))
+except ZeroDivisionError as zde:
+    print("Oops a zero divided was thrown.")
+    print("Error type: ", type(zde))
 except Exception:
-    print("Oops an exception was thrown.")
+    print("well this was unexpected.")
 print("outside the try/except block")
