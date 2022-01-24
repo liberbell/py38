b30f3761b63e0810b14693cce508c667984f9471
Liberbell <liberbell@gmail.com>
2022-01-24 09:14:36 +0900

    errors5

--- errors.py
+++ errors.py
@@ -2,10 +2,18 @@
 # print("hello" + 3)
 # print(does_not_exit + 3)
 
+# try:
+#     user_input = input("Please input an integer: ")
+#     result = 100 / int(user_input)
+#     print("100 divided by the integer you input: ", result)
+# except ValueError:
+#     print("Oops an exception was thrown.")
+# print("outside the try/except block")
+
 try:
     user_input = input("Please input an integer: ")
     result = 100 / int(user_input)
     print("100 divided by the integer you input: ", result)
-except ValueError:
+except (ValueError, ZeroDivisionError):
     print("Oops an exception was thrown.")
 print("outside the try/except block")
