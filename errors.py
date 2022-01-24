ae7985ced322a5223ea809f87355506d093b7d5e
Liberbell <liberbell@gmail.com>
2022-01-24 09:17:45 +0900

    errors7

--- errors.py
+++ errors.py
@@ -10,10 +10,22 @@
 #     print("Oops an exception was thrown.")
 # print("outside the try/except block")
 
+# try:
+#     user_input = input("Please input an integer: ")
+#     result = 100 / int(user_input)
+#     print("100 divided by the integer you input: ", result)
+# except (ValueError, ZeroDivisionError):
+#     print("Oops an exception was thrown.")
+# print("outside the try/except block")
+
 try:
     user_input = input("Please input an integer: ")
     result = 100 / int(user_input)
     print("100 divided by the integer you input: ", result)
-except (ValueError, ZeroDivisionError):
-    print("Oops an exception was thrown.")
+except ValueError as ve:
+    print("Oops a Value error was thrown.")
+    print("Error type: ", type(ve))
+except ZeroDivisionError as zde:
+    print("Oops a zero divided was thrown.")
+    print("Error type: ", type(zde))
 print("outside the try/except block")
