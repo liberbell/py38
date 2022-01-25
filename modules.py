a6e7b7b1ca6d50780f4c7615f6824226981c6c92
Liberbell <liberbell@gmail.com>
2022-01-25 09:16:51 +0900

    modules6

--- modules.py
+++ modules.py
@@ -1,6 +1,8 @@
 import imp
 import math
 import os
+from os import path
+import pwd
 
 print("Pi: ", math.pi)
 print("e: ", math.e)
@@ -9,4 +11,7 @@ print(math.pow(3, 4))
 print(os.getcwd())
 print(os.listdir())
 
-os.mkdir("./osmodule")
+# os.mkdir("./osmodule")
+
+# print(path.isdir(cwd))
+print(path.isdir("./osmodule"))
