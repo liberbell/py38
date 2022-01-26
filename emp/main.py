ed04c0f86e134d914faa9792a57320e5331e25e8
Liberbell <liberbell@gmail.com>
2022-01-26 14:42:40 +0900

    main7

--- emp/main.py
+++ emp/main.py
@@ -12,5 +12,13 @@ class Employee:
         print(f"Salary: {self.__salary}")
         print(f"Organization: {Employee.__organization}")
 
-employee_denvor = Employee("Denvor", "HR", 67000)
-employee_denvor.display_detail()
+# employee_denvor = Employee("Denvor", "HR", 67000)
+# employee_denvor.display_detail()
+def create_employee(name, department, salary):
+    employee = Employee(name, department, salary)
+    return employee
+
+if __name__ == "__main__":
+    print("**********", __name__)
+    employee_denvor = Employee("Denvor", "HR", 67000)
+    employee_denvor.display_detail()
