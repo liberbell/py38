e3a4ec48d45247926a9102bd41f951659b0af544
Liberbell <liberbell@gmail.com>
2022-01-27 08:33:56 +0900

    main3

--- parse/main.py
+++ parse/main.py
@@ -3,6 +3,14 @@ import sys
 from employee import create_employee
 
 if __name__ == "__main__":
-    print("Number of command line arguments: ", len(sys.argv))
-    print("Script name: ", sys.argv[0])
-    print("All arguments: ", sys.argv)
+    # print("Number of command line arguments: ", len(sys.argv))
+    # print("Script name: ", sys.argv[0])
+    # print("All arguments: ", sys.argv)
+    print("All argument: ", sys.argv)
+
+    name = sys.argv[1]
+    department = sys.argv[2]
+    salary = int(sys.argv[3])
+
+    emplo = create_employee(name, department, salary)
+    emplo.display_detail()
