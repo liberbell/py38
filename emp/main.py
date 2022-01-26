1da4a4c89e1dee850e83e2a7de6848afc2639ec7
Liberbell <liberbell@gmail.com>
2022-01-26 14:49:35 +0900

    man9

--- emp/main.py
+++ emp/main.py
@@ -1,10 +1,11 @@
 import employee
 
 def create_employee(name, department, salary):
-    employee = Employee(name, department, salary)
+    # employee = Employee(name, department, salary)
+    employee = employee.Employee(name, department, salary)
     return employee
 
 if __name__ == "__main__":
     print("**********", __name__)
-    employee_denvor = Employee("Denvor", "HR", 67000)
+    employee_denvor = employee.Employee("Denvor", "HR", 67000)
     employee_denvor.display_detail()
