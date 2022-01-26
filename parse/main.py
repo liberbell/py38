e930e55972b82540342ad96148623a9e6ad25f36
Liberbell <liberbell@gmail.com>
2022-01-27 08:29:25 +0900

    main1

--- parse/main.py
+++ parse/main.py
@@ -2,6 +2,6 @@ from ast import If
 import sys
 
 if __name__ == "__main__":
-    print("Number of command line arguments: ")
+    print("Number of command line arguments: ", len(sys.argv))
     print("Script name: ", sys.argv[0])
     print("All arguments: ", sys.argv)
