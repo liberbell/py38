ced5075f0803c9803b10db4b2dc1c6175b32301d
Liberbell <liberbell@gmail.com>
2022-01-26 14:58:20 +0900

    main12

--- emp/main.py
+++ emp/main.py
@@ -1,7 +1,7 @@
 from employee import Employee, create_employee
 
 if __name__ == "__main__":
-    print("**********", __name__)
+    print("In main **********", __name__)
     employee_denvor = create_employee("Denvor", "HR", 67000)
     employee_denvor.display_detail()
     employee_jessica = create_employee("Jessica", "Sales", 76000)
