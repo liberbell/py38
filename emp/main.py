2605d45cd9d09f9ac178b0c56fd7957dd4921448
Liberbell <liberbell@gmail.com>
2022-01-26 14:46:25 +0900

    main7

--- emp/main.py
+++ emp/main.py
@@ -1,19 +1,3 @@
-class Employee:
-    __organization = "somewhere"
-
-    def __init__(self, name, department, salary):
-        self.__name = name
-        self.__department = department
-        self.__salary = salary
-
-    def display_detail(self):
-        print(f"Name: {self.__name}")
-        print(f"Department: {self.__department}")
-        print(f"Salary: {self.__salary}")
-        print(f"Organization: {Employee.__organization}")
-
-# employee_denvor = Employee("Denvor", "HR", 67000)
-# employee_denvor.display_detail()
 def create_employee(name, department, salary):
     employee = Employee(name, department, salary)
     return employee
