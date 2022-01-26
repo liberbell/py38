d4cbfe272c8730f8db1ba9ffc420c0af5a25e74f
Liberbell <liberbell@gmail.com>
2022-01-26 21:39:06 +0900

    main13

--- emp/main.py
+++ emp/main.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     print("In main **********", __name__)
     employee_denvor = create_employee("Denvor", "HR", 67000)
     employee_denvor.display_detail()
-    employee_jessica = create_employee("Jessica", "Sales", 76000)
-    employee_jessica.display_detail()
+    # employee_jessica = create_employee("Jessica", "Sales", 76000)
+    # employee_jessica.display_detail()
