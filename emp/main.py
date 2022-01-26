ee50799b6408e06b65408d8adcf48120d25aede6
Liberbell <liberbell@gmail.com>
2022-01-26 14:35:55 +0900

    main3

--- emp/main.py
+++ emp/main.py
@@ -7,3 +7,7 @@ class Employee:
         self.__salary = salary
 
     def display_detail(self):
+        print(f"Name: {self.__name}")
+        print(f"Department: {self.__department}")
+        print(f"Salary: {self.__salary}")
+        print(f"Organization: {Employee.__organization}")
