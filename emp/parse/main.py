f463559d60c175655a6a1ebab2cb3ddb3e13de1a
Liberbell <liberbell@gmail.com>
2022-01-26 21:57:50 +0900

    main4

--- emp/parse/main.py
+++
@@ -1,6 +0,0 @@
-from ast import If
-import sys
-
-if __name__ == "__main__":
-    print("Number of command line arguments: ", )
-    print("Script name: ", sys.argv[0])
---
+++ parse/main.py
@@ -0,0 +1,7 @@
+from ast import If
+import sys
+
+if __name__ == "__main__":
+    print("Number of command line arguments: ")
+    print("Script name: ", sys.argv[0])
+    print("All arguments: ", sys.argv)
