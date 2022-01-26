1df1d78c5a045c3b3baf10675f8240754cf4a677
Liberbell <liberbell@gmail.com>
2022-01-27 08:31:39 +0900

    employee1

---
+++ parse/employee.py
@@ -0,0 +1,17 @@
+class Employee:
+    __organization = "somewhere"
+
+    def __init__(self, name, department, salary):
+        self.__name = name
+        self.__department = department
+        self.__salary = salary
+
+    def display_detail(self):
+        print(f"Name: {self.__name}")
+        print(f"Department: {self.__department}")
+        print(f"Salary: {self.__salary}")
+        print(f"Organization: {Employee.__organization}")
+
+def create_employee(name, department, salary):
+    employee = Employee(name, department, salary)
+    return employee
